59d84a8b82e4e3c8e0b6f087269b8d3e80ce0593
Brian Bensky <brianbensky@gmail.com>
2019-07-31 15:21:23 -0600

    added variables for ami

--- ami.tf
+++ ami.tf
@@ -3,10 +3,10 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   # THis is Canonical
-  owners = ["099720109477"]
+  owners = ["${var.ami_owner_id}"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["${var.ami_filter_value}"]
   }
 }
