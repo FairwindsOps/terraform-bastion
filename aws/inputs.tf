404fbd080d9cc8e153e19f760829127df4da5a00
Andrew Suderman <andrew@sudermanjr.com>
2020-08-11 11:59:25 -0600

    Remove aws provider and optional s3 bucket region var

--- aws/inputs.tf
+++ aws/inputs.tf
@@ -7,12 +7,6 @@ variable "infrastructure_bucket" {
   description = "An S3 bucket to store data that should persist on the bastion when it is recycled by the Auto Scaling Group, such as SSH host keys. This can be set in the environment via `TF_VAR_infrastructure_bucket`"
 }
 
-variable "infrastructure_bucket_region" {
-  type        = string
-  description = "The S3 bucket region"
-  default     = null
-}
-
 variable "infrastructure_bucket_bastion_key" {
   description = "The key; sub-directory in $infrastructure_bucket where the bastion will be allowed to read and write. Do not specify a trailing slash. This allows sharing an S3 bucket among multiple invocations of this module."
   default     = "bastion"
--- aws/s3.tf
+++ aws/s3.tf
@@ -1,20 +1,11 @@
 data "aws_region" "current" {}
 
-provider "aws" {
-  alias  = "bastion_state"
-  region = local.infrastructure_bucket_region
-  endpoints {
-    s3 = local.infrastructure_bucket_s3_endpoint
-  }
-}
-
 locals {
   infrastructure_bucket             = data.aws_s3_bucket.infrastructure_bucket
-  infrastructure_bucket_region      = var.infrastructure_bucket_region == null ? data.aws_region.current.name : var.infrastructure_bucket_region
-  infrastructure_bucket_s3_endpoint = var.infrastructure_bucket_region == data.aws_region.current.name ? format("https://s3.%s.amazonaws.com", data.aws_region.current.name) : format("https://s3.%s.amazonaws.com", local.infrastructure_bucket_region)
+  infrastructure_bucket_region      = data.aws_region.current.name
+  infrastructure_bucket_s3_endpoint = format("https://s3.%s.amazonaws.com", data.aws_region.current.name)
 }
 
 data "aws_s3_bucket" "infrastructure_bucket" {
-  provider = aws.bastion_state
   bucket   = var.infrastructure_bucket
 }
