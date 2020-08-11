80f4b13f076a3c8a01483792d95537df068e4b57
Andrew Suderman <andrew@sudermanjr.com>
2020-08-11 12:19:11 -0600

    remove unused locals

--- aws/s3.tf
+++ aws/s3.tf
@@ -2,8 +2,6 @@ data "aws_region" "current" {}
 
 locals {
   infrastructure_bucket             = data.aws_s3_bucket.infrastructure_bucket
-  infrastructure_bucket_region      = data.aws_region.current.name
-  infrastructure_bucket_s3_endpoint = format("https://s3.%s.amazonaws.com", data.aws_region.current.name)
 }
 
 data "aws_s3_bucket" "infrastructure_bucket" {
