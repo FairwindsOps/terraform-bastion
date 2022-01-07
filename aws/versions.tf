7b2fe75943720e46da0af9a0dbb891864a0ebe16
Sam McAulay <22226554+sammc3@users.noreply.github.com>
2022-01-07 15:18:14 -0700

    modify provider config for tf version >=0.13

--- aws/versions.tf
+++ aws/versions.tf
@@ -1,7 +1,17 @@
 
+# terraform {
+#   required_version = ">= 0.12"
+#   required_providers {
+#     aws = ">=2.30.0"
+#   }
+# }
+
 terraform {
-  required_version = ">= 0.12"
+  version = ">= 0.13"
   required_providers {
-    aws = ">=2.30.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=2.30.0"
+    }
   }
 }
--- gcp/versions.tf
+++ gcp/versions.tf
@@ -1,8 +1,18 @@
 
+# terraform {
+#   required_version = ">= 0.12"
+#   required_providers {
+#     google   = ">=2.0.0"
+#     template = ">=2.1.2"
+#   }
+# }
+
 terraform {
-  required_version = ">= 0.12"
+  version = ">= 0.13"
   required_providers {
-    google   = ">=2.0.0"
-    template = ">=2.1.2"
+    google = {
+      source  = "hashicorp/google"
+      version = ">=2.0.0"
+    }
   }
 }
